9e048a0523dbf9a30f8c53e46500a2f50fd81c45
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2019-11-13 18:19:03 +0300

    feat: add mainNav content with js and styles

---
+++ src/blocks/mainNav/mainNav.js
@@ -0,0 +1,33 @@
+const CLOSE = `mainNav__burger--close`;
+const MAIN_NAV_ITEM_FLEX = `mainNav__item--mobile-flex`;
+const mainNav = document.querySelector(`.mainNav`);
+const burger = mainNav.querySelector(`.mainNav__burger`);
+const mobileItems = mainNav.querySelectorAll(`.mainNav__item--mobile`);
+
+const viewItems = () => {
+  mobileItems.forEach((item) => {
+    item.classList.add(MAIN_NAV_ITEM_FLEX);
+  });
+};
+
+const unviewItems = () => {
+  mobileItems.forEach((item) => {
+    item.classList.remove(MAIN_NAV_ITEM_FLEX);
+  });
+};
+
+const closeNav = () => {
+  burger.classList.remove(CLOSE);
+  burger.removeEventListener(`click`, closeNav);
+  unviewItems();
+};
+
+const dropNav = () => {
+  burger.classList.add(CLOSE);
+  burger.addEventListener(`click`, closeNav);
+  viewItems();
+};
+
+if (burger) {
+  burger.addEventListener(`click`, dropNav);
+}
