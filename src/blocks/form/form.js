8a4813c06ac5bacb8ee0fde0c69dac27dd377c6a
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2019-12-09 12:43:25 +0300

    correct: layout through pages, form.js main idea

--- src/blocks/form/form.js
+++ src/blocks/form/form.js
@@ -1,11 +1,54 @@
 const body = document.querySelector(`body`);
 const overlay = body.querySelector(`.overlay`);
-const form = body.querySelector(`.form--promo`);
+const getPrice = body.querySelectorAll(`.button--getPrice`);
+const callMe = body.querySelectorAll(`.button--callMe`);
+
+const form = body.querySelector(`.form`);
+
 const textarea = form.querySelector(`.form__input--textarea`);
 const textareaHidden = form.querySelector(`#form__input--textarea-hidden`);
 const telInput = form.querySelector(`.form__input--tel`);
 const getPriceSubmit = form.querySelector(`.button--getPrice-submit`);
-const getPrice = body.querySelectorAll(`.button--getPrice`);
+
+const formPrice = `
+  <form class="form form--promo" action="/submit" method="post">
+    <fieldset class="form__fieldset">
+      <label class="form__label">
+        <span class="text text--light-grey text--sm">Ваше имя</span>
+        <input type="text" class="form__input" id="form_name" name="form_name" placeholder="Иван Иванов" required>
+      </label>
+      <label class="form__label">
+        <span class="text text--light-grey text--sm">Телефон</span>
+        <input type="tel" class="form__input form__input--tel" id="form_phone" name="form_phone" placeholder="+7(999)123-1415" required>
+      </label>
+      <label class="form__label">
+        <span class="text text--light-grey text--sm">E-mail</span>
+        <input type="email" class="form__input" id="form_email" name="form_email" placeholder="ivan.ivanov@mail.ru" required>
+      </label>
+        <button class="button button--action button--center button--getPrice-submit" type="submit">Получить прайс</button>
+    </fieldset>
+  </form>
+  `;
+
+const formCallMe = `
+  <form class="form form--callMe" action="/submit" method="post">
+    <fieldset class="form__fieldset">
+      <label class="form__label">
+        <span class="text text--light-grey text--sm">Ваше имя</span>
+        <input type="text" class="form__input" id="form_name" name="form_name" placeholder="Иван Иванов" required>
+      </label>
+      <label class="form__label">
+        <span class="text text--light-grey text--sm">Телефон</span>
+        <input type="tel" class="form__input form__input--tel" id="form_phone" name="form_phone" placeholder="+7(999)123-1415" required>
+      </label>
+      <label class="form__label">
+        <span class="text text--light-grey text--sm">E-mail</span>
+        <input type="email" class="form__input" id="form_email" name="form_email" placeholder="ivan.ivanov@mail.ru" required>
+      </label>
+        <button class="button button--action button--center button--getPrice-submit" type="submit">Получить прайс</button>
+    </fieldset>
+  </form>
+  `;
 
 if (textarea) {
   textarea.addEventListener(`input`, () => {
@@ -30,10 +73,8 @@ const telInputValidate = (input) => {
       return;
     } else {
       telInput.setCustomValidity(``);
-    }
-    
+    } 
   }
-
 };
 
 if (telInput) {
@@ -58,12 +99,25 @@ const formOpen = () => {
   overlay.style.display = `block`;
   overlay.addEventListener(`click`, formClose);
   body.style.overflow = `hidden`;
-  form.style.display = `block`;
   form.addEventListener(`submit`, formSubmitEventListener);
 }
 
+const openPromoForm = () => {
+
+};
+
+const openCallMeForm = () => {
+
+};
+
 if (getPrice) {
   getPrice.forEach((button) => {
-    button.addEventListener(`click`, formOpen);
+    button.addEventListener(`click`, openPromoForm);
+  })
+}
+
+if (callMe) {
+  callMe.forEach((button) => {
+    button.addEventListener(`click`, openCallMeForm);
   })
 }
